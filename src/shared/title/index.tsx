102edcad2d3dd9c9fed8e60b8faa28fcef07ff0c
Olaroungbe Jamiu <88253089+Abiodun-code@users.noreply.github.com>
2025-02-20 14:43:49 +0100

    Update index.tsx

--- src/shared/title/index.tsx
+++ src/shared/title/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, TouchableOpacity } from 'react-native';
 import { Text } from 'react-native-paper';
-import {  MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { hp } from '@/utils/responsiveHelper';
 
@@ -18,22 +18,23 @@ interface Props {
   iconP?: number | undefined;
   iconR?: number | undefined;
   iconColor?: string | undefined;
+  width?: number | any;
 }
 
-const Title = ({ children, font, variant, color, showIcon = false, textA, textT, iconSize, iconBg, iconP, iconR, iconColor }: Props) => {
+const Title = ({ children, font, variant, color, showIcon = false, textA, textT, iconSize, iconBg, iconP, iconR, iconColor, width }: Props) => {
   const navigation = useNavigation();
 
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+    <View style={{ flexDirection: 'row', alignItems: 'center', width: width }}>
       {showIcon && (
-        <TouchableOpacity onPress={() => navigation.goBack()} style={{backgroundColor: iconBg, padding: iconP, borderRadius: iconR}}>
+        <TouchableOpacity onPress={() => navigation.goBack()} style={{ backgroundColor: iconBg, padding: iconP, borderRadius: iconR }}>
           <MaterialIcons name="arrow-back-ios-new" size={iconSize || hp(3)} color={iconColor || "black"} />
         </TouchableOpacity>
       )}
 
       <Text
-        variant={variant || 'titleMedium'}
-        style={{ fontFamily: font || 'i700', color: color, textAlign: textA || 'center', flex: 1, textTransform: textT }}
+        variant={variant}
+        style={{ fontFamily: font, color: color, textAlign: textA || 'center', flex: 1, textTransform: textT }}
       >
         {children}
       </Text>

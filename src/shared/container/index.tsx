91c6d7c722eefa13c2862a254db11f717eaa140f
Olaroungbe Jamiu <88253089+Abiodun-code@users.noreply.github.com>
2025-05-09 21:15:31 +0100

    Update index.tsx

--- src/shared/container/index.tsx
+++ src/shared/container/index.tsx
@@ -1,21 +1,32 @@
-import { View, ViewProps } from 'react-native';
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import {Colors} from '@/constants/Colors';
+import React from 'react';
+import { View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Colors } from '@/constants/Colors';
 
 interface Props {
-  children: React.ReactNode,
-  bgColor?: string,
-  padX?: number,
-}
-const Container = ({ children, bgColor, padX }: Props) => {
-  return (
-    <View style={{ flex: 1, backgroundColor: bgColor || Colors.white, paddingHorizontal: padX }}>
-      <SafeAreaView style={{flex:1}}>
-        {children}
-      </SafeAreaView>
-    </View>
-  )
+  children: React.ReactNode;
+  bgColor?: string;
+  padX?: number;
+  padT?: number;
+  padB?: number;
 }
 
-export default Container
+const Container = ({ children, bgColor, padX = 0, padT = 0, padB = 0 }: Props) => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: bgColor || Colors.white,
+        paddingTop: padT + insets.top,
+        paddingBottom: padB + insets.bottom,
+        paddingHorizontal: padX,
+      }}
+    >
+      {children}
+    </View>
+  );
+};
+
+export default Container;

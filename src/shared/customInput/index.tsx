8fecce066ca3b43ae8361406f8f8c00a10e955bb
Olaroungbe Jamiu <88253089+Abiodun-code@users.noreply.github.com>
2025-05-09 21:16:57 +0100

    Update index.tsx

--- src/shared/customInput/index.tsx
+++ src/shared/customInput/index.tsx
@@ -1,34 +1,50 @@
-import { View } from 'react-native'
-import React from 'react'
-import { TextInput, TextInputProps } from 'react-native-paper'
-import {Colors} from '@/constants/Colors'
-import { hp } from '@/utils/responsiveHelper'
+import { View } from 'react-native';
+import React from 'react';
+import { TextInput, TextInputProps } from 'react-native-paper';
+import { Colors } from '@/constants/Colors';
+import { hp } from '@/utils/responsiveHelper';
 
 interface CustomTextProps extends TextInputProps {
-  label?: string,
-  disabled?: boolean,
-  mb?: number
-  bg?: any,
-  bgColor?: any,
+  label?: string;
+  disabled?: boolean;
+  mb?: number;
+  bg?: any;
+  bgColor?: any;
+  width?: number | string; // new prop
+  textColor?: string
 }
-const CustomInput: React.FC<CustomTextProps> = ({
+
+const Input: React.FC<CustomTextProps> = ({
   label,
   disabled,
   mb,
   bg,
   bgColor,
+  width,
+  textColor,
   ...props
 }) => {
-
   return (
-    <View style={{ marginBottom: mb || hp(2.5), flexDirection:'row' }}>
-      <TextInput label={label} style={{ backgroundColor: bgColor || Colors.white, fontSize: hp(1.9), flex:1 }}
-        outlineStyle={{ borderRadius: hp(1.5), borderColor: bg || Colors.grey }} mode='outlined'
-        textColor={Colors.black} activeOutlineColor={Colors.black} disabled={disabled}
+    <View style={{ marginBottom: mb || hp(2.5), width: width ?? '100%' }}>
+      <TextInput
+        label={label}
+        style={{
+          backgroundColor: bgColor || Colors.white,
+          fontSize: hp(1.9),
+        }}
+        outlineStyle={{
+          borderRadius: hp(1.2),
+          borderColor: bg || Colors.grey,
+          borderWidth: hp(0.1),
+        }}
+        mode="outlined"
+        textColor={textColor || Colors.black}
+        activeOutlineColor={Colors.black}
+        disabled={disabled}
         {...props}
       />
     </View>
-  )
-}
+  );
+};
 
-export default CustomInput
+export default Input;

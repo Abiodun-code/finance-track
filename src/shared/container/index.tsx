bae595cb32b206c3aa6eee4c22a8782781a44d3c
Olaroungbe Jamiu <88253089+Abiodun-code@users.noreply.github.com>
2025-06-07 10:20:10 +0100

    Update index.tsx

--- src/shared/container/index.tsx
+++ src/shared/container/index.tsx
@@ -1,4 +1,4 @@
-import { View, ViewProps } from 'react-native';
+import { Platform, View, ViewProps } from 'react-native';
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Colors } from '@/constants/Colors';
@@ -12,12 +12,20 @@ interface Props {
 }
 const Container = ({ children, bgColor, padX, padT }: Props) => {
   return (
-    <View style={{ flex: 1, backgroundColor: bgColor || Colors.white, paddingHorizontal: padX, paddingTop:padT, marginBottom:hp(-4)}}>
-      <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: bgColor || Colors.white, paddingTop: padT, paddingHorizontal: padX, paddingBottom: Platform.OS === 'ios' ? hp(-3.7) : hp(-7), }}>
+      {/* <View
+        style={{
+          flex: 1,
+          paddingHorizontal: padX,
+          paddingTop: padT,
+          // If you want some spacing at the bottom, use paddingBottom
+          
+        }}
+      > */}
         {children}
-      </SafeAreaView>
-    </View>
-  )
-}
+      {/* </View> */}
+    </SafeAreaView>
+  );
+};
 
 export default Container
